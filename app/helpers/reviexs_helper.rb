25c8bce0863e8e3c712759559f9bad6346ad41fa
Arthur Ribe <arthur-ribe@hotmail.fr>
2024-04-13 14:57:09 -0400

    rails new with bootstrap & simple_form

---
+++ app/helpers/reviexs_helper.rb
@@ -0,0 +1,2 @@
+module ReviexsHelper
+end
---
+++ db/migrate/20240413182036_rename_column_adress_to_address.rb
@@ -0,0 +1,5 @@
+class RenameColumnAdressToAddress < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :restaurants, :adress, :address
+  end
+end
