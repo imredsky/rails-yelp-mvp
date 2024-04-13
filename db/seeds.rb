23957eb4ad3c2d459fb19e21c1a9e562f2eb414e
Arthur Ribe <arthur-ribe@hotmail.fr>
2024-04-13 17:01:31 -0400

    ex2

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "3145663102", category: "french"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St London E1 6PQ", phone_number: "4588651452", category: "italian"}
+sushi_sakura = {name: "Sushi Sakura", address: "458 Rue Laurier, Montréal", phone_number: "4588651762", category: "japanese"}
+des_frites = {name: "Des Frites", address: "7859 Rue Sainte Catherine Est, Montréal", phone_number: "4588614756", category: "belgian"}
+beijing_dumpling = {name: "Beijing Dumpling", address: "1452 Avenue Sherbrooke, Montréal", phone_number: "1245624785", category: "chinese"}
+
+[dishoom, pizza_east, sushi_sakura, des_frites, beijing_dumpling].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
